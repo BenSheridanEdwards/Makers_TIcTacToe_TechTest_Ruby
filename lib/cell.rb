dccb17fba91b29c042443d11c59c19bc3b9a7b79
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-29 17:45:51 +0000

    Add empty method to cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,2 +1,10 @@
 class Cell
+
+  def initialize
+    @contents = "-"
+  end
+
+  def empty?
+    return true if @contents == "-"
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,2 +1,11 @@
+require 'cell'
+
 describe Cell do
+  let(:cell) { Cell.new }
+
+  describe '#empty?' do
+    it "tells the game if the cell is empty for a player to fill with their symbol" do
+      expect(cell.empty?).to eq true
+    end
+  end
 end
